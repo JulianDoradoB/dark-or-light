8cd1eaa89d2a2e843be8642eff990e69b70637a3
Julian dorado <julian.dorado@campusucc.edu.co>
2024-11-06 09:23:26 -0500

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,74 @@ import ThemeSwitch from './components/ThemeSwitch';
 
 export default function Home() {
   return (
-    <main className='flex items-center justify-center w-screen h-screen bg-cyan-200 dark:bg-cyan-900'>
-      <section className='flex flex-col items-center gap-y-10'>
+    <main className="w-full h-screen flex flex-col justify-between">
+      <section className="flex flex-col items-center justify-center w-full h-full">
         <ThemeSwitch />
-        <h1 className='text-6xl font-extrabold text-cyan-900 dark:text-cyan-200'>
-          Hi, mode dark in Next.js!
-        </h1>
+        <div className="w-full h-full flex items-center justify-center">
+          <div className="w-full h-full flex items-center justify-center">
+            <div className="relative isolate overflow-hidden bg-ColorInicio-ColorLIma-300 dark:bg-ColorInicio-900 w-full h-auto px-6 shadow-2xl sm:rounded-3xl sm:px-16 lg:flex lg:gap-x-20 lg:px-24">
+              <svg
+                viewBox="0 0 1024 1024"
+                className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+                aria-hidden="true"
+              >
+                <circle
+                  cx="512"
+                  cy="512"
+                  r="512"
+                  fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+                  fillOpacity="0.7"
+                />
+                <defs>
+                  <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                    <stop stopColor="#7775D6" />
+                    <stop offset="1" stopColor="#E935C1" />
+                  </radialGradient>
+                </defs>
+              </svg>
+              <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+                <h2 className="text-balance text-3xl dark:font-[family-name:var(--font-rubik-wet)] tracking-tight dark:text-ColorInicio-300 text-white sm:text-4xl">
+                  Boost your productivity. Start using our app today.
+                </h2>
+                <p className="mt-6 text-pretty text-lg/8 dark:text-ColorInicio-300 text-white dark:font-[family-name:var(--font-tiny-5)]">
+                  Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                  <a
+                    href="#"
+                    className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                  >
+                    Get started
+                  </a>
+                  <a href="#" className="text-sm/6 font-semibold text-white">
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+              <div className="relative lg:mt-8">
+                <img
+                  className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+                  src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png"
+                  alt="App screenshot"
+                  width="1824"
+                  height="1080"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
+      <footer className="bg-gray-900 dark:bg-gray-800 py-4">
+        <div className="flex justify-center">
+          <a href="#">
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Company logo"
+            />
+          </a>
+        </div>
+      </footer>
     </main>
   );
 }
